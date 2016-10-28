590e13f1a2946ebb3b6e0ced038cbe3b2f0801a9
wujincun <1769453779@qq.com>
2016-10-28 17:16:50 +0800

    配置

--- webpack.vue.config.js
+++ webpack.vue.config.js
@@ -36,7 +36,12 @@ module.exports = {
             {
                 test: /\.(png|jpg)$/,
                 loader: 'url-loader?limit=8192' //图片文件使用 url-loader 来处理，小于8kb的直接转为base64
-            }
+            },
+            /*{
+                test: require.resolve('jquery'),//在 AMD/CMD 中，我们需要对不符合规范的模块（比如一些直接返回全局变量的插件）进行 shim 处理，这时候我们需要使用 exports-loader 来帮忙
+                //之后在脚本中需要引用该模块的时候，这么简单地来使用就可以了：require('./tool/swipe.js');swipe();
+                loader: 'expose?jQuery'
+            }//输出jQuery到全局*/
         ]
         //noParse:/no-parse.js/,//使用了noParse的模块将不会被loaders解析,即此处的no-parse.js文件太大，并且其中不包含require、define或者类似的关键字的时候(因为这些模块加载并不会被解析，所以就会报错)，我们就可以使用这项配置来提升性能
     },
@@ -58,8 +63,8 @@ module.exports = {
     plugins:[
         new htmlWebpackPlugin({
             title:"欢迎",
-            filename:"main.html",//生成的html文件名称，如果不写，则默认为index.html
-            //chunks:["build"],//html中引入的js模块
+            //filename:"main.html",//生成的html文件名称，如果不写，则默认为index.html
+            chunks:["build"],//html中引入的js模块
              //template: "./build_vue/index.html",//path.resolve('index.html'),//模板路径
             inject: 'body'
         }),
@@ -67,13 +72,13 @@ module.exports = {
             title:"欢迎",
             filename:"class.html",//生成的html文件名称，如果不写，则默认为index.html
             chunks:["abs"]
-        }),*/
-        //new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js"),//提取出公共文件vendor---》vendor.bundle.js
+        }
+        //new webpack.optimize.CommonsChunkPlugin(/* chunkName= *///"vendor", /* filename= */"vendor.bundle.js"),//提取出公共文件vendor---》vendor.bundle.js
         //new webpack.optimize.CommonsChunkPlugin("common.js"),//抽取多入口文件的js公共部分
         //new ExtractTextPlugin("[name].css"),//css文件提取出来
-       // new ExtractTextPlugin("style.css", { allChunks: true }),//css合并到一个文件
+       // neExtractTextPlugin("style.css", { allChunks: true }),//css合并到一个文件
        /* new webpack.optimize.CommonsChunkPlugin("commons", "commons.js"), //如果包含chunk文件，并且chunk文件中也引入了样式文件，样式文件不会嵌入到js中，而是直接输出到style.css
-        new ExtractTextPlugin("[name].css")*/
+        new //ExtractTextPlugin("[name].css")*/
 
 
     ]
